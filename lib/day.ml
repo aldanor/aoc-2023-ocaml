adbcc799aab571860604709d91d9206e61f9eccd
Ivan Smirnov <github@ivan.smirnov.ie>
2023-12-02 02:57:34 +0000

    Add simple mtime elapsed time printing

--- lib/day.ml
+++ lib/day.ml
@@ -15,7 +15,11 @@ end
 module Make (Impl : Impl) : S = struct
   let run ?(only_part1 = false) ?(only_part2 = false) inputs =
     let parsed = Impl.parse inputs in
-    let () = if not only_part2 then Impl.part1 parsed in
-    let () = if not only_part1 then Impl.part2 parsed in
+    let () =
+      if not only_part2 then Imports.time_m (fun () -> Impl.part1 parsed)
+    in
+    let () =
+      if not only_part1 then Imports.time_m (fun () -> Impl.part2 parsed)
+    in
     ()
 end
--- lib/imports.ml
+++ lib/imports.ml
@@ -9,6 +9,24 @@ let time f =
   Stdlib.print_endline (Printf.sprintf "%f" (after -. before)) ;
   result
 
+let fmt_elapsed_nice ns_total =
+  let ns = ns_total % 1_000_000_000 in
+  let s = ns_total / 1_000_000_000 in
+  let ms_f = Float.of_int ns /. 1_000_000. in
+  let fmt = Printf.sprintf "%02ds %.3fms" in
+  fmt s ms_f
+
+let time_m f =
+  let before = Mtime_clock.elapsed () in
+  let result = f () in
+  let after = Mtime_clock.elapsed () in
+  let elapsed_ns =
+    Mtime.Span.abs_diff before after
+    |> Mtime.Span.to_uint64_ns |> Int64.to_int_exn
+  in
+  Stdlib.print_endline (fmt_elapsed_nice elapsed_ns) ;
+  result
+
 let parse_digit c =
   match c with
   | '0' .. '9' -> Some (Char.to_int c - Char.to_int '0')
