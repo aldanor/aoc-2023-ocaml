a9868112e63fed5d0252e7b1c4d1e5d222e83a96
Ivan Smirnov <github@ivan.smirnov.ie>
2023-12-04 15:18:55 +0000

    Day 04 - part 1 refactor

--- lib/day04.ml
+++ lib/day04.ml
@@ -2,6 +2,45 @@ open Imports
 open Base
 open Core
 
+module Game = struct
+  type t =
+    { s: string
+    ; n_win: int
+    ; n_num: int
+    ; n_lines: int
+    ; n_pre: int
+    ; line_len: int
+    ; arr: int array }
+
+  let create s =
+    let n_pre = str_find_char_exn s ':' + 2 in
+    let n_win = (str_find_char_exn s '|' - n_pre) / 3 in
+    let n_num = ((str_find_char_exn s '\n' - n_pre - 1) / 3) - n_win in
+    let line_len = 1 + (1 + n_pre + (3 * (n_win + n_num))) in
+    let n_lines = (1 + String.length s) / line_len in
+    let arr = Array.create ~len:100 (-1) in
+    {s; n_win; n_num; n_lines; n_pre; line_len; arr}
+
+  let n_win g line_id =
+    let digit i = String.unsafe_get g.s i |> parse_digit_unchecked in
+    let parse_num2 i = (10 * (digit i |> Int.max 0)) + digit (i + 1) in
+    let i = ref ((line_id * g.line_len) + g.n_pre) in
+    for _ = 0 to g.n_win - 1 do
+      let k = parse_num2 !i in
+      Array.unsafe_set g.arr k line_id ;
+      i := !i + 3
+    done ;
+    let n_win = ref 0 in
+    let i = ref ((line_id * g.line_len) + g.n_pre + 2 + (3 * g.n_win)) in
+    for _ = 0 to g.n_num - 1 do
+      let k = parse_num2 !i in
+      let v = Array.unsafe_get g.arr k in
+      if v = line_id then n_win := !n_win + 1 ;
+      i := !i + 3
+    done ;
+    !n_win
+end
+
 module M = struct
   type t = string
 
@@ -9,31 +48,11 @@ module M = struct
 
   let part1 s =
     (* 15268 *)
-    let n_pre = str_find_char_exn s ':' + 2 in
-    let n_win = (str_find_char_exn s '|' - n_pre) / 3 in
-    let n_num = ((str_find_char_exn s '\n' - n_pre - 1) / 3) - n_win in
-    let line_len = 1 + (1 + n_pre + (3 * (n_win + n_num))) in
-    let n_lines = (1 + String.length s) / line_len in
-    let digit i = String.unsafe_get s i |> parse_digit_unchecked in
-    let parse_num i = (10 * (digit i |> Int.max 0)) + digit (i + 1) in
+    let g = Game.create s in
     let ans = ref 0 in
-    let arr = Array.create ~len:100 (-1) in
-    for line_id = 0 to n_lines - 1 do
-      let start = (line_id * line_len) + n_pre in
-      for win_id = 0 to n_win - 1 do
-        let j = start + (3 * win_id) in
-        let k = parse_num j in
-        Array.unsafe_set arr k line_id
-      done ;
-      let start = start + 2 + (3 * n_win) in
-      let score = ref 0 in
-      for num_id = 0 to n_num - 1 do
-        let j = start + (3 * num_id) in
-        let k = parse_num j in
-        let v = Array.unsafe_get arr k in
-        if v = line_id then score := Int.max 1 (!score * 2)
-      done ;
-      ans := !ans + !score
+    for line_id = 0 to g.n_lines - 1 do
+      let n_win = Game.n_win g line_id in
+      if not (n_win = 0) then ans := !ans + Int.shift_left 1 (n_win - 1)
     done ;
     !ans |> Int.to_string
 
@@ -50,4 +69,4 @@ let%expect_test _ =
    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n\
    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n\
    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11" |> run_test ;
-  [%expect {| 13 |}]
+  [%expect {| 13 30 |}]
