003ed0dc6b9fc9a94062b060f934fd13959188aa
Ivan Smirnov <github@ivan.smirnov.ie>
2023-12-05 02:19:30 +0000

    Day 01 - 2x more faster part 2

--- lib/day01.ml
+++ lib/day01.ml
@@ -68,26 +68,56 @@ module M = struct
 
   let part2 s =
     (* 55343 *)
-    let nan = -1 in
+    let unwrap_safe x = Option.value x ~default:(-42) in
+    let get_char = String.unsafe_get s in
     let digit c = Char.to_int c - Char.to_int '0' in
-    let num2 x y = (10 * x) + y in
-    let total, first, last = (ref 0, ref nan, ref nan) in
-    for i = 0 to String.length s - 1 do
-      match String.unsafe_get s i with
-      | '0' .. '9' as c ->
-          last := digit c ;
-          if !first = nan then first := !last
-      | '\n' ->
-          total := !total + num2 !first !last ;
-          first := nan
-      | _ -> (
-        match match_trie s i with
-        | Some j ->
-            last := j ;
-            if !first = nan then first := j
-        | None -> () )
+    let n = String.length s in
+    let i, eof, total = (ref 0, ref false, ref 0) in
+    (* process a single line at a time *)
+    while not !eof do
+      (* step 1, scan for the first digit (eol/eof will not be hit) *)
+      let first = ref None in
+      while Option.is_none !first do
+        match get_char !i with
+        | '0' .. '9' as c -> first := Some (digit c)
+        | _ -> (
+          match match_trie s !i with
+          | Some j -> first := Some j
+          | None -> incr i )
+      done ;
+      incr i ;
+      let start = ref !i in
+      let first_num = unwrap_safe !first in
+      (* step 2, scan to the end of the line (or eof), keep track of
+         digits *)
+      let eol = ref None in
+      while Option.is_none !eol do
+        if !i = n then (
+          eof := true ;
+          eol := Some !i )
+        else if Char.equal (get_char !i) '\n' then eol := Some !i
+        else incr i
+      done ;
+      (* step 3, scan the end of the line for possible digits *)
+      let start = !start in
+      let eol = unwrap_safe !eol in
+      let j = ref (eol - 1) in
+      let last_num = ref None in
+      while Option.is_none !last_num && !j >= start do
+        match get_char !j with
+        | '0' .. '9' as c -> last_num := Some (digit c)
+        | _ -> (
+          match match_trie s !j with
+          | Some k -> last_num := Some k
+          | None -> decr j )
+      done ;
+      (* step 4, add the number and jump to the next line *)
+      let last_num = Option.value !last_num ~default:first_num in
+      total := !total + (10 * first_num) + last_num ;
+      i := eol + 1 ;
+      ()
     done ;
-    !total + num2 !first !last |> Int.to_string
+    !total |> Int.to_string
 end
 
 include M
