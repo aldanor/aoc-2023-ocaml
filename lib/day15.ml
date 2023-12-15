9d8f33f5b4d2b93c6710cb69d5ea261b761bce1e
Ivan Smirnov <github@ivan.smirnov.ie>
2023-12-15 12:17:49 +0000

    Day 15 - part 1

--- lib/day15.ml
+++ lib/day15.ml
@@ -1,3 +1,4 @@
+open Core
 open! Imports
 
 module M = struct
@@ -5,7 +6,13 @@ module M = struct
 
   let parse s = s
 
-  let part1 _ = ""
+  let part1 s =
+    let f (sum, hash) c =
+      match c with
+      | ',' -> (sum + hash, 0)
+      | c -> (sum, (hash + Char.to_int c) * 17 mod 256)
+    in
+    String.fold s ~init:(0, 0) ~f |> (fun (a, b) -> a + b) |> Int.to_string
 
   let part2 _ = ""
 end
@@ -13,4 +20,8 @@ end
 include M
 include Day.Make (M)
 
-let%expect_test _ = "" |> run_test ; [%expect {| |}]
+let%expect_test _ = "HASH" |> run_test ; [%expect {| 52 |}]
+
+let%expect_test _ =
+  "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7" |> run_test ;
+  [%expect {| 1320 |}]
