b355b594e79b5457bd97671efc72cfe6ea128926
Ivan Smirnov <github@ivan.smirnov.ie>
2023-12-16 20:33:13 +0000

    Day 16 - part 2

--- lib/day16.ml
+++ lib/day16.ml
@@ -1,10 +1,13 @@
 open Core
-open! Imports
 
 module Board = struct
   type t = {s: string; v: bytes; w: int; h: int}
 
   type state = {x: int; y: int; pos: int; dir: int}
+
+  let width b = b.w - 1
+
+  let height b = b.h
 
   let of_string s =
     let w = String.index_exn s '\n' in
@@ -14,7 +17,7 @@ module Board = struct
     Bytes.fill v ~pos:0 ~len:(n + 1) '\x00' ;
     {s; v; w= w + 1; h}
 
-  let start = {x= 0; y= 0; pos= 0; dir= 1}
+  let start {w; _} x y dir = {x; y; pos= (y * w) + x; dir}
 
   let move w {x; y; pos; dir} =
     match dir with
@@ -40,7 +43,9 @@ module Board = struct
     done ;
     print_endline ""
 
-  let run {s; v; w; h} =
+  let reset {v; _} = Bytes.fill v ~pos:0 ~len:(Bytes.length v) '\x00'
+
+  let run {s; v; w; h} start =
     (* nesw = 0123 *)
     (* reverse = 2301 = (dir + 2) mod 4 *)
     (* \ = wsen = 3210 = 3 - dir *)
@@ -48,18 +53,21 @@ module Board = struct
     (* | := dir if dir == 0|2 else [/, \] *)
     (* - := dir if dir == 1|3 else [/, \] *)
     (* ord(|) mod 2 = 1, ord(-) mod 2 = 0 *)
-    let rec f queue =
+    let rec f queue terminal =
       match queue with
-      | [] -> ()
-      | {x; y; _} :: qs when x < 0 || x >= w - 1 || y < 0 || y >= h -> f qs
+      | [] -> terminal
+      | {x; y; _} :: qs when x < 0 -> f qs ((3, y) :: terminal) (* w *)
+      | {x; y; _} :: qs when x >= w - 1 -> f qs ((1, y) :: terminal) (* e *)
+      | {x; y; _} :: qs when y < 0 -> f qs ((0, x) :: terminal) (* n *)
+      | {x; y; _} :: qs when y >= h -> f qs ((2, x) :: terminal) (* s *)
       | {pos; dir; _} :: qs
-        when Char.to_int (Bytes.get v pos) land (1 lsl dir) <> 0 ->
-          f qs
+        when Char.to_int (Bytes.unsafe_get v pos) land (1 lsl dir) <> 0 ->
+          f qs terminal
       | ({pos; dir; _} as state) :: qs ->
-          Bytes.set v pos
+          Bytes.unsafe_set v pos
           @@ Char.unsafe_of_int
-               (Char.to_int (Bytes.get v pos) lor (1 lsl dir)) ;
-          let c = String.get s pos in
+               (Char.to_int (Bytes.unsafe_get v pos) lor (1 lsl dir)) ;
+          let c = String.unsafe_get s pos in
           let move_fwd () = move w {state with dir= (5 - dir) land 3} in
           let move_rev () = move w {state with dir= 3 - dir} in
           let queue =
@@ -68,13 +76,11 @@ module Board = struct
             | '/' -> move_fwd () :: qs
             | ('-' | '|') when dir land 1 <> Char.to_int c land 1 ->
                 move_rev () :: move_fwd () :: qs
-            (* | '-' when dir = 0 || dir = 2 -> move_rev () :: move_fwd () :: qs *)
-            (* | '|' when dir = 1 || dir = 3 -> move_rev () :: move_fwd () :: qs *)
             | _ -> move w state :: qs
           in
-          f queue
+          f queue terminal
     in
-    f [start]
+    f [start] []
 
   let n_energized {v; _} =
     Bytes.fold v ~init:0 ~f:(fun acc c ->
@@ -88,11 +94,36 @@ module M = struct
 
   let part1 s =
     (* 7472 *)
-    let b = Board.of_string s in
-    Board.run b ;
-    Board.n_energized b |> Int.to_string
+    let module B = Board in
+    let b = B.of_string s in
+    B.run b (B.start b 0 0 1) |> ignore ;
+    B.n_energized b |> Int.to_string
 
-  let part2 _ = ""
+  let part2 s =
+    (* 7716 *)
+    let module B = Board in
+    let b = B.of_string s in
+    let w, h = (B.width b, B.height b) in
+    let starts =
+      [| Array.init w ~f:(fun x -> Some (B.start b x 0 2))
+       ; Array.init h ~f:(fun y -> Some (B.start b (w - 1) y 3))
+       ; Array.init w ~f:(fun x -> Some (B.start b x (h - 1) 0))
+       ; Array.init h ~f:(fun y -> Some (B.start b 0 y 1)) |]
+    in
+    let n_max = ref 0 in
+    for i = 0 to 3 do
+      for j = 0 to Array.length starts.(i) - 1 do
+        match starts.(i).(j) with
+        | Some start ->
+            B.reset b ;
+            let terminal = B.run b start in
+            List.iter terminal ~f:(fun (dir, loc) ->
+                starts.(dir).(loc) <- None ) ;
+            n_max := max !n_max (B.n_energized b)
+        | None -> ()
+      done
+    done ;
+    !n_max |> Int.to_string
 end
 
 include M
@@ -109,4 +140,4 @@ let%expect_test _ =
    .-.-/..|..\n\
    .|....-|.\\\n\
    ..//.|...." |> run_test ;
-  [%expect {| 46 |}]
+  [%expect {| 46 51 |}]
